24fc1367f4b42efd2c55a7cd04f9dfbcf9dc374e
GEETHAprakash <35689211+GEETHAprakash@users.noreply.github.com>
2018-02-23 16:07:13 +0530

    Create prakash75.c

---
+++ prakash75.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a, b, c,d;
+printf("Enter two numbers to add\n");
+scanf("%d%d", &a, &b,&c);
+d = a + b +c;
+printf("Sum of the numbers = %d\n", d);
+getch();
+  }

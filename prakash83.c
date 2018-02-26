1ea060d41dc6cb9b543c529941019af74cb10a0b
GEETHAprakash <35689211+GEETHAprakash@users.noreply.github.com>
2018-02-26 13:24:41 +0530

    Rename prakash82.c to prakash83.c

---
+++ prakash83.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+int x, y, temp;
+scanf("%d%d", &x, &y);
+printf("\nx = %d\ny = %d\n",x,y);
+temp = x;
+x    = y;
+y    = temp;
+printf("\nx = %d\ny = %d\n",x,y);
+return 0;
+}

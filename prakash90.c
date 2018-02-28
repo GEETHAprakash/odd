cef90ee0a56bed7854d834a2a31bb8dfaa0ccac0
GEETHAprakash <35689211+GEETHAprakash@users.noreply.github.com>
2018-02-28 14:29:14 +0530

    prakash90.c

---
+++ prakash90.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#inlcude<conio.h>
+void main()
+{
+int n,i;
+int count=0;
+printf("enter the number");
+scanf("%d",&n);
+for(i=2;i<=n;i++)
+{
+if((n%i)==0)
+{
+count++;
+}
+}
+if(count==1)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+  getch();
+}

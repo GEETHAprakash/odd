855e04d6a9dd733c71da247c99c656ea9e7e1862
GEETHAprakash <35689211+GEETHAprakash@users.noreply.github.com>
2018-02-28 14:16:50 +0530

    prakash88.c

---
+++ prakash88.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int x;
+    for (x = 0; x < 10; x++)
+        if (x % 2)
+            printf("%d is odd\n", x);
+    return 0;
+}
+
+/* and.c */
+#include <stdio.h>
+
+int main(void)
+{
+    int x;
+    for (x = 0; x < 10; x++)
+        if (x & 1)
+            printf("%d is odd\n", x);
+    getch();
+}

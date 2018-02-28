3eefd0c29b60ff40005e7eb2d6a3ce442cd14df0
GEETHAprakash <35689211+GEETHAprakash@users.noreply.github.com>
2018-02-28 14:52:40 +0530

    prakash92.c

---
+++ prakash92.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int a;
+  scanf("%d",&a);
+  if(a%7==0)
+  printf("\n Multiple of 7");
+  else
+  printf("\n Not Multiple of 7");  
+  return 0;
+}

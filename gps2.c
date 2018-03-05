0b457766a53c38e7197624fdc4d154b995e70ff9
GEETHAprakash <35689211+GEETHAprakash@users.noreply.github.com>
2018-03-05 13:31:29 +0530

    gps2.c

---
+++ gps2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+clrscr();
+printf("Enter the Number:");
+scanf("%d",&a);
+if(a%13==0)
+{
+printf("Yes");
+}
+else
+printf("No");
+return 0;
+}

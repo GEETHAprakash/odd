7bd407d3e8d9d869435329fbe619a30e92f1a4ac
GEETHAprakash <35689211+GEETHAprakash@users.noreply.github.com>
2018-03-23 16:04:05 +0530

    prrk13.c

---
+++ prrk13.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int L,B,H;
+scanf(“%d %d %d”,&L,&B,&H);
+SA=(2*L*B)+(2*L*H)+(2*H*B);
+VOL=(L*B*H);
+printf(“volume=%d  surface area=%d”,VOL,SA);
+getch();
+}
